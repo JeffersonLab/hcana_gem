f43c68fa8087c25ddb4ed51ea09442fb90545cd7
Stephen A. Wood <saw@jlab.org>
2017-07-25 16:25:57 -0400

    In GEMEvtHandler::Analyze call Decode and Update   Use pointer to data instead of copying

--- src/GEMEvtHandler.cc
+++ src/GEMEvtHandler.cc
@@ -23,20 +23,37 @@ GEMEvtHandler::~GEMEvtHandler()
 }
 Int_t GEMEvtHandler::Analyze(THaEvData *evdata)
 {
-  cout << "Handling GEM event" << endl;
+  if( !IsMyEvent(evdata->GetEvType()) ) return -1;
+
+  Int_t ndata = evdata->GetEvLength();
+  UInt_t* rdata = (UInt_t*) evdata->GetRawDataBuffer();
+  
+  //  cout << "GEMEvtHandler::Analyze " << ndata << " " << rdata[0] << " " << 
+  //   evtype << endl;
+  // Decode says it want's length, but what it really wants is index
+  // of last word of event
+  fHandler->Decode(&rdata[2],ndata-3);
+  fUpdateEvent->Update();
 
   return kOK;
 }
 THaAnalysisObject::EStatus GEMEvtHandler::Init(const TDatime& date)
 {
+  fStatus = kOK;
+
   // Will want to open the GEM configuration files here
   GEMConfigure* configure = new GEMConfigure(fConfigFileName.c_str());
   configure->LoadConfigure();
 
   string mapping_file(configure->GetMapping());
-  cout<<"Loading Mapping File from:  "<<mapping_file.c_str()<<endl;
+  cout<<"Loading GEM Mapping File from:  "<<mapping_file.c_str()<<endl;
   GEMMapping* mapping = GEMMapping::GetInstance();
   mapping->LoadMapping(mapping_file.c_str());
+
+  fGEMAnalyzer = new GEMAnalyzer(configure);
+  fParser = fGEMAnalyzer->GetParser();
+  fHandler = fGEMAnalyzer->GetHandler();
+  fUpdateEvent = fParser->GetEventUpdater();
 
   return kOK;
 }
--- src/GEMEvtHandler.h
+++ src/GEMEvtHandler.h
@@ -8,6 +8,10 @@
 /////////////////////////////////////////////////////////////////////
 
 #include "THaEvtTypeHandler.h"
+#include "GEMAnalyzer.h"
+#include "GEMEvioParser.h"
+#include "GEMDataHandler.h"
+#include "EventUpdater.h"
 
 class GEMEvtHandler : public THaEvtTypeHandler {
 
@@ -17,13 +21,17 @@ public:
    virtual ~GEMEvtHandler();
 
    Int_t Analyze(THaEvData *evdata);
-   virtual void AddEventType(Int_t evtype);
    virtual EStatus Init( const TDatime& run_time);
    void SetConfigFile( const char* name) { fConfigFileName = name; }
 
 protected:
 
    std::string fConfigFileName;
+
+   GEMAnalyzer* fGEMAnalyzer;
+   GEMEvioParser* fParser;
+   GEMDataHandler* fHandler;
+   EventUpdater* fUpdateEvent;
 
    ClassDef(GEMEvtHandler,0)
 
